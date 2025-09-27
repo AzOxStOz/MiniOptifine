4a39594bbf6c497ce09ae672b4a5aa9596b17869
AzOxStOz <117990481+AzOxStOz@users.noreply.github.com>
2025-09-28 01:28:39 +0200

    J'regarde pas les streams (streams) j'regarde pas les ventes (ventes)

--- MiniOptifine/Loader.cpp
+++ MiniOptifine/Loader.cpp
@@ -9,8 +9,8 @@ void init() {
     uint32_t process_id = l->getGameProcessId();
     uintptr_t gameId = l->getGameModule(process_id);
     HANDLE process_handle = OpenProcess(PROCESS_ALL_ACCESS, NULL, process_id);
-    uintptr_t optifineZoombaseAddress = gameId + 0x053DC110;
-    uintptr_t __optifineZoomPtr = l->findAddressFromPointer(process_handle, optifineZoombaseAddress, { 0x10, 0x18, 0x58, 0x8, 0x10, 0x180, 0x18 });
+    uintptr_t optifineZoombaseAddress = gameId + 0x09024EA8;
+    uintptr_t __optifineZoomPtr = l->findAddressFromPointer(process_handle, optifineZoombaseAddress, { 0x20, 0x8, 0x198, 0x18 });
            
     std::cout << "Checking if game window is present...\n\n";
     // check if the minecraft window is even open
@@ -36,7 +36,7 @@ void init() {
                 WriteProcessMemory(process_handle, (unsigned int*)__optifineZoomPtr, &zoomVal, sizeof(zoomVal), nullptr);
                 
             } else {
-                //std::cout << std::dec << defaultFov << "\n";
+                std::cout << std::dec << defaultFov << "\n";
                 WriteProcessMemory(process_handle, (unsigned int*)__optifineZoomPtr, &defaultFov, sizeof(defaultFov), nullptr);
             }
         }
